459918b68f9785ee70540612ca6b2769a865b81e
Svetlin Mladenov <sveem@users.noreply.github.com>
2018-06-06 10:08:30 -0400

    Added ajax.get call to Wikipedia api

--- viewmodel.js
+++ viewmodel.js
@@ -2,12 +2,24 @@ var app = app || {};
 
 app.vm = (function() {
   var title = ko.observable('Wikipedia-Viewer');
-
-
+  var searchTerm = '';
+  
+  function searchPage(term) {
+    console.log('searchPage', term);
+    var url = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&
+    gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max
+    &gsrsearch=${term}&callback=JSON_CALLBACK`;
+    $.get(url, (data) => {
+      console.log('Wikipedia-Data', JSON.parse(data));  
+    })
+  }
+  
   var vm = {
-    title: title  
+    title: title,
+    searchTerm: searchTerm,
+    searchPage: searchPage  
   };
-
+ 
   return vm;
 }());
 
